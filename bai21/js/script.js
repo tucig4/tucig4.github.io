2e2e2761566b50b7ba5c629b1d249135f3c10a8d
tucig4 <36256777+tucig4@users.noreply.github.com>
2019-02-18 03:11:45 +0700

    fix

--- bai21/js/script.js
+++ bai21/js/script.js
@@ -1,27 +1,68 @@
-function includeHTML() {
-        var z, i, elmnt, file, xhttp;
-        /* Loop through a collection of all HTML elements: */
-        z = document.getElementsByTagName("*");
-        for (i = 0; i < z.length; i++) {
-          elmnt = z[i];
-          /*search for elements with a certain atrribute:*/
-          file = elmnt.getAttribute("tu-include-html");
-          if (file) {
-            /* Make an HTTP request using the attribute value as the file name: */
-            xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
-              if (this.readyState == 4) {
-                if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-                if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-                /* Remove the attribute, and call this function once more: */
-                elmnt.removeAttribute("tu-include-html");
-                includeHTML();
-              }
-            } 
-            xhttp.open("GET", file, true);
-            xhttp.send();
-            /* Exit the function: */
-            return;
-          }
-        }
+cards=['img\2D__57497.1440113502.480.480.png',
+'img\3C__99122.1440113509.480.480.png',
+'img\4H__83243.1440113515.480.480.png',
+'img\5S__90574.1440113521.480.480.png',
+'img\KH__01216.1440113580.480.480.png'];
+
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
+
+var check=false;
+$('.card').click(function(){
+    if(check===false){
+        $(this).toggleClass('clicked');
+        check=true;
+    }
+});
+function checkClassClicked(){
+    if(check===true){
+        $('.card').click(function(){
+           $(this).toggleClass('clicked');
+         });
+    }
+}
+var arrayOfImage=[];
+arrayOfImage.push("img/2D__57497.1440113502.480.480.png");
+arrayOfImage.push("img/3C__99122.1440113509.480.480.png");
+arrayOfImage.push("img/4H__83243.1440113515.480.480.png");
+arrayOfImage.push("img/5S__90574.1440113521.480.480.png");
+arrayOfImage.push("img/KH__01216.1440113580.480.480.png");
+arrayOfImage.push("img/2D__57497.1440113502.480.480.png");
+arrayOfImage.push("img/3C__99122.1440113509.480.480.png");
+arrayOfImage.push("img/4H__83243.1440113515.480.480.png");
+arrayOfImage.push("img/5S__90574.1440113521.480.480.png");
+arrayOfImage.push("img/KH__01216.1440113580.480.480.png");
+function replaceAllImageSrc(){
+    shuffle(arrayOfImage);
+    $("#one").attr("src",arrayOfImage[0]);
+    $("#two").attr("src",arrayOfImage[1]);
+    $("#three").attr("src",arrayOfImage[2]);
+    $("#four").attr("src",arrayOfImage[3]);
+    $("#five").attr("src",arrayOfImage[4]);
+    $("#six").attr("src",arrayOfImage[5]);
+    $("#seven").attr("src",arrayOfImage[6]);
+    $("#eight").attr("src",arrayOfImage[7]);
+    $("#nine").attr("src",arrayOfImage[8]);
+    $("#ten").attr("src",arrayOfImage[9]);
+}
+replaceAllImageSrc();
+
+
+
